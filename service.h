c49469be0215ef4c8b823ba38d8871c9b93503cb
DaniRadu-s <daniradu374@gmail.com>
2025-03-19 14:08:31 +0200

    Delete service.h

--- service.h
+++
@@ -1,8 +0,0 @@
-
-int valideaza(Buget *p, MyList* l);
-
-void testValidare();
-
-void modificare(MyList *l, char* tip, int zi, float suma,  char* type, int day, float sum);
-
-void test_modificare();
